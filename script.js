a8236c5df61bb9d7bef26d3c5b35dd650ddcfa3f
unknown <Z20gab@gmail.com>
2022-01-10 14:01:48 +0100

    added files

---
+++ script.js
@@ -0,0 +1,42 @@
+// 1
+
+var age = 16;
+var boy = true;
+var interrest = "baseball";
+
+//2
+
+var firstname = "Gabriel";
+var lastname = "zouad";
+
+var fullname =
+  firstname + " " + lastname;
+("");
+console.log(fullname);
+
+//3
+
+var typeoffrog = typeof "frog";
+console.log(
+  "This type of frog is a " +
+    typeoffrog +
+    "."
+);
+
+//4
+
+var orderHasShipped = false;
+
+if (orderHasShipped == true) {
+  console.log("The Order has Shipped");
+} else {
+  console.log(
+    "The Order has Not Shipped"
+  );
+}
+
+//5
+
+for (var i = 7; i < 14; i++) {
+  console.log(i);
+}
